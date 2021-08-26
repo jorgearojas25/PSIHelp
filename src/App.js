761954298450fa30325cb9387f5ce91bc6bb77c1
Jorge <jorgearojas25@gmail.com>
2021-08-26 12:49:53 -0500

    project config

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Soy la app!</Text>
+    </View>
+  )
+}
+
+export default App
